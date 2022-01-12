820fdfe785806aedec517555cce6e7bee57b8d1e
Siham Hadi <siham.hadi@live.se>
2022-01-12 12:49:14 +0100

    Add Icon componenet

---
+++ src/components/icons/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import OverviewIcon from './OverviewIcon.svelte';
+	import FactoryIcon from './FactoryIcon.svelte';
+	import RecycleIcon from './RecycleIcon.svelte';
+	import ImpactIcon from './ImpactIcon.svelte';
+
+	export let name = '';
+</script>
+
+{#if name === 'overview'}
+	<OverviewIcon color="#6e7378" width={22} height={22} />
+{/if}
+{#if name === 'sites'}
+	<FactoryIcon color="#6e7378" width={20} height={20} />
+{/if}
+{#if name === 'treatment'}
+	<RecycleIcon color="#6e7378" width={22} height={22} />
+{/if}
+{#if name === 'impact'}
+	<ImpactIcon color="#6e7378" width={28} height={28} />
+{/if}
