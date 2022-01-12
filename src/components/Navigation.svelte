700f2b474f5577de36b86132f98ce28e369461be
Siham Hadi <siham.hadi@live.se>
2022-01-12 12:48:58 +0100

    Add navigation

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+	import Overview from '../routes/overview.svelte';
+	import Sites from '../routes/sites.svelte';
+	import Recycle from '../routes/treatment.svelte';
+	import Impact from '../routes/impact.svelte';
+
+	export const navOptions = [
+		{ page: 'overview', component: Overview },
+		{ page: 'sites', component: Sites },
+		{ page: 'treatment', component: Recycle },
+		{ page: 'impact', component: Impact }
+	];
+</script>
