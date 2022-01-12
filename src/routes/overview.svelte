2ccb29502a1874610502559dd670fb217614c51f
Siham Hadi <siham.hadi@live.se>
2022-01-12 15:22:54 +0100

    Add highlight card

--- src/routes/overview.svelte
+++ src/routes/overview.svelte
@@ -1,1 +1,24 @@
+<script>
+	import HighlightCard from '../components/HighlightCard.svelte';
+</script>
+
 <h2>Overview</h2>
+<br />
+<br />
+<div class="container">
+	<HighlightCard title={'Financial risk projection'} currency={'$'} number={1001011} />
+	<HighlightCard title={'Recycled'} number={1000000} unit={'ytd'} />
+	<HighlightCard title={'Impact Score'} symbol={'🔺'} number={60} />
+</div>
+
+<style>
+	h2 {
+		font-size: 2.4rem;
+	}
+	.container {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		width: 80%;
+	}
+</style>
