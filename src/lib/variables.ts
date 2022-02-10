5c1ed658230991bb1314768285fa2689bd699324
Siham Hadi <siham.hadi@live.se>
2022-02-10 08:56:04 +0100

    Add env varibles

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,3 +1,5 @@
 export const varibales = {
-	apiKey: import.meta.env.VITE_API_KEY
+	apiKey: import.meta.env.VITE_API_KEY,
+	domain: import.meta.env.VITE_DOMAIN,
+	clientId: import.meta.env.VITE_CLIENT_ID
 };
