08cab027e17eae8e03fa8f7087596637c1e887d4
Siham Hadi <siham.hadi@live.se>
2022-02-10 09:08:28 +0100

    Add simple error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script>
+	import '../global.css';
+</script>
+
+<section>
+	<h1>Sorry, nothing to see here</h1>
+	<a href="/">Go back</a>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		background-color: #0a1015;
+		color: #f9fafb;
+	}
+	a {
+		padding: 1rem;
+		text-decoration: none;
+		list-style: none;
+		color: #f9fafb;
+	}
+</style>
