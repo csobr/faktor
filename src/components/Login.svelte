85335500ae8d9fd90f9532b1f43a972551120d0e
Siham Hadi <siham.hadi@live.se>
2022-02-10 08:56:43 +0100

    Add sign in page

---
+++ src/components/Login.svelte
@@ -0,0 +1,68 @@
+<script>
+	import '../global.css';
+	import SocialButton from './SocialButton.svelte';
+
+	export let error = '';
+</script>
+
+<svelte:head>
+	<title>Sign in</title>
+</svelte:head>
+<div class="wrapper">
+	<section>
+		<img class="logo" src="logo_light.svg" alt="logo" />
+		<div class="inner">
+			<h1>Sign in to Faktor</h1>
+			<p>{error}</p>
+			<SocialButton text="Continue with Google" on:click />
+		</div>
+	</section>
+	<div class="divider">
+		<img src="depth.svg" alt="circle" />
+	</div>
+</div>
+
+<style>
+	.logo {
+		height: 7.5rem;
+	}
+	h1 {
+		font-size: 4.8rem;
+		font-weight: 700;
+		margin: 0;
+	}
+	.wrapper {
+		display: flex;
+		height: 100vh;
+	}
+	img {
+		width: auto;
+		padding: 3rem;
+	}
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 20%;
+		width: 50vw;
+	}
+	.inner {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+		height: 17rem;
+		justify-content: space-between;
+	}
+	.divider {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		right: 0;
+		background-color: #0e171f;
+		height: 100%;
+		width: 50vw;
+		position: absolute;
+		z-index: 0;
+	}
+</style>
---
+++ src/components/SocialButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let text: string = '';
+</script>
+
+<button on:click>
+	<img src="google.svg" alt="google_icon" />
+	<slot>{text}</slot>
+</button>
+
+<style>
+	img {
+		padding-right: 1.5rem;
+	}
+	button {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		padding: 1.5rem 4.5rem;
+		border: 0;
+		border-radius: 0.4rem;
+		border: 0.1rem solid #0e171f;
+		background-color: #f9fafb;
+		font-size: 1.6rem;
+		font-weight: 700;
+		cursor: pointer;
+	}
+	@media (max-width: 991.98px) {
+		button {
+			padding: 1.4rem 1.4rem;
+		}
+	}
+</style>
