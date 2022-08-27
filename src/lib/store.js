acf1250336f07073beb72590beb57e429b383024
Siham Hadi <siham.hadi@live.se>
2022-08-27 20:43:43 +0200

    Remove unused files

--- src/lib/store.js
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const isAuthenticated = writable(false);
-export const user = writable({});
-export const openPopup = writable(false);
-export const setError = writable('');
-export const organizations = writable([]);
-export const organization = writable([]);
--- src/lib/variables.ts
+++
@@ -1,6 +0,0 @@
-export const varibales = {
-	apiKey: import.meta.env.VITE_API_KEY,
-	domain: import.meta.env.VITE_DOMAIN,
-	clientId: import.meta.env.VITE_CLIENT_ID,
-	access_token: import.meta.env.VITE_ACCESS_TOKEN
-};
