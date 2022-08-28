1479805aacbe9adfdc9419a1eedf0417e4a14be9
Siham Hadi <siham.hadi@live.se>
2022-08-28 10:14:33 +0200

    Update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
-import preprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess(),
 
 	kit: {
-		adapter: vercel(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: vercel()
 	}
 };
 
