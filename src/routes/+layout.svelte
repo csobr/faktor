84d325c9e3d437510cbf6fb702682c9d148faaae
Siham Hadi <siham.hadi@live.se>
2022-08-30 22:29:01 +0200

    Add analytics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  '../global.css';
+</script>
+
+<svelte:head>
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id={import.meta.env.VITE_ANALYTICS_ID}"></script>
+</svelte:head>
+<slot />
