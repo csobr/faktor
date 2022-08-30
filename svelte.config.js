e7463f7c4bd090dbe2b4ca5ea6b2e697a0c1da30
Siham Hadi <siham.hadi@live.se>
2022-08-30 22:27:49 +0200

    Migrate new sveltekit changes

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,11 @@ import sveltePreprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
 
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+  preprocess: sveltePreprocess(),
 
-	kit: {
-		adapter: vercel()
-	}
+  kit: {
+    adapter: vercel()
+  }
 };
 
 export default config;
