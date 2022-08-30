75af6fc9fefc7f2a8cebdc47d2ab35b5c79f5487
Siham Hadi <siham.hadi@live.se>
2022-08-30 22:23:56 +0200

    Delete files

--- src/components/CTA_Button.svelte
+++ src/components/CTA_Button.svelte
@@ -1,37 +1,38 @@
 <script lang="ts">
-	export let background: string = '';
-	export let color: string = '';
-	export let border: string = '';
+  export let background: string = '';
+  export let color: string = '';
+  export let border: string = '';
 </script>
 
 <button
-	on:click
-	data-tf-popup="HdbmLNpA"
-	data-tf-iframe-props="title=Contact"
-	style="background-color:{background};color:{color};border:{border};"
-	><slot>Request a product demo</slot>
-	<script src="//embed.typeform.com/next/embed.js"></script>
+  on:click
+  data-tf-popup="HdbmLNpA"
+  data-tf-iframe-props="title=Contact"
+  style="background-color:{background};color:{color};border:{border};"
+  ><slot>Request a product demo</slot>
+  <script src="//embed.typeform.com/next/embed.js"></script>
 </button>
 
 <style>
-	button {
-		border: 0;
-		z-index: 1;
-		border-radius: 0.4rem;
-		font-weight: 600;
-		display: flex;
-		flex-direction: row;
-		align-items: flex-start;
-		padding: 1.8rem 2.2rem;
-		font-size: 1.4rem;
-		font-weight: 700;
-		cursor: pointer;
-		margin: 0 auto;
-	}
-	@media (max-width: 991.98px) {
-		button {
-			padding: 1.4rem 1.4rem;
-			font-size: 1rem;
-		}
-	}
+  button {
+    border: 0;
+    z-index: 1;
+    position: relative;
+    border-radius: 0.4rem;
+    font-weight: 600;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 1.8rem 2.2rem;
+    font-size: 1.4rem;
+    font-weight: 700;
+    cursor: pointer;
+    margin: 0 auto;
+  }
+  @media (max-width: 991.98px) {
+    button {
+      padding: 1.4rem 1.4rem;
+      font-size: 1rem;
+    }
+  }
 </style>
